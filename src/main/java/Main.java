4432ebe0222a8267b1238c2707a6e312d0ec975f
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-22 23:12:44 -0500

    Removed the Polymorphic class and added a method to main to accomplish the same thing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,27 @@ public class Main {
 
         exam2.setScore(70);
         System.out.println(exam2.getGrade());
+        GradedActivity[] exams = Polymorphic();
+        for (int i = 0; i < exams.length; i++) {
+            System.out.println("Test " + (i + 1) + ": "+
+                    "score " + exams[i].getScore() +
+                    ", grade " + exams[i].getGrade());
+        }
+    }
 
+    /**
+     * Creates an array of GradedActivity objects
+     * @return An array of GradedActivity Objects
+     */
+    public static GradedActivity[] Polymorphic() {
+        GradedActivity[] tests = new GradedActivity[3];
+
+        tests[0] = new GradedActivity();
+        tests[0].setScore(95);
+
+        tests[1] = new PassFailExam(20,5,60);
+
+        tests[2] = new FinalExam(50,7);
+        return tests;
     }
 }
