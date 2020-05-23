1103c60d1f445c860434cde67e2f62868157bdfc
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-23 01:03:37 -0500

    Created a sample interface

---
+++ src/main/java/Displayable.java
@@ -0,0 +1,8 @@
+/*
+* At its core an interface is like a class that only contains abstract
+* methods. An interface cannot be instantiated, it can only be
+* implemented by other classes. The classes that implement the interface
+*  must override methods of the interface*/
+public interface Displayable {
+    void display();
+}
