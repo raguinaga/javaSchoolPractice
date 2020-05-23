57aab9698aefaa6ac85948b23d500ff36a04c54e
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-22 23:00:56 -0500

    Added some demos of polymorphism

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello gradle!");
+
+        GradedActivity exam1 = new FinalExam(50,7);
+        GradedActivity exam2 = new PassFailActivity(70);
+        GradedActivity exam3 = new PassFailExam(100,10,70);
+
+        exam2.setScore(70);
+        System.out.println(exam2.getGrade());
+
     }
 }
