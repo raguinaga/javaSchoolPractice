a1c2db3e133b964e25eaffff953cd169ea4682c7
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-23 00:42:12 -0500

    Created a class to create a CS student object. Very hacky workaround to just creating the object in Main, but I am reusing main, so this code would otherwise be erased, here it will be stored.

---
+++ src/main/java/CompSciDemo.java
@@ -0,0 +1,32 @@
+public class CompSciDemo {
+    // Make a CS student var.
+    CompSciStudent csStudent;
+
+    /**
+     * This constructor sets up a CS Student
+     * object, it's a hacky workaround for the fact that
+     * I already have a main method, and I have to create
+     * an object to create another object. I could just make a
+     * CS student object in the main method but then I would have to
+     * replace all the code eventually, this is for now, a more
+     * permanent solution.
+     */
+    public CompSciDemo() {
+        //Create a CompSciStudent object
+        csStudent = new CompSciStudent("Jennifer" +
+                "Haynes", "167W98337", 2015);
+
+        // Set values for student hours
+        csStudent.setMathHours(12);
+        csStudent.setCsHours(20);
+        csStudent.setGenEdHours(40);
+    }
+
+    /**
+     * Returns a fully set up CS student object
+     * @return A ref to a CS Student object
+     */
+    public CompSciStudent getCsStudent() {
+        return csStudent;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        CompSciDemo demo = new CompSciDemo();
+        CompSciStudent student1 = demo.getCsStudent();
 
-        GradedActivity exam1 = new FinalExam(50,7);
-        GradedActivity exam2 = new PassFailActivity(70);
-        GradedActivity exam3 = new PassFailExam(100,10,70);
+        System.out.println(student1);
 
-        exam2.setScore(70);
-        System.out.println(exam2.getGrade());
-        GradedActivity[] exams = Polymorphic();
-        for (int i = 0; i < exams.length; i++) {
-            System.out.println("Test " + (i + 1) + ": "+
-                    "score " + exams[i].getScore() +
-                    ", grade " + exams[i].getGrade());
-        }
-    }
-
-    /**
-     * Creates an array of GradedActivity objects
-     * @return An array of GradedActivity Objects
-     */
-    public static GradedActivity[] Polymorphic() {
-        GradedActivity[] tests = new GradedActivity[3];
-
-        tests[0] = new GradedActivity();
-        tests[0].setScore(95);
-
-        tests[1] = new PassFailExam(20,5,60);
-
-        tests[2] = new FinalExam(50,7);
-        return tests;
+        System.out.println("Hours remaining: " +
+                student1.getRemainingHours());
     }
 }
