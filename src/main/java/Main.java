0409d4277d9709ef3d187432bb42fc7a1d776752
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-23 01:50:59 -0500

    Added some comments to main on Interface polymorphism

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person("Robert Aguinaga");
-
-        p.display();
+        // Fun fact, when two objects implement the same interface you
+        // can create them using the interface as the variable type.
+        // Note that this is mostly useless after Java introduced the var
+        // keyword.
+        RetailItem item1 = new CompactDisc("New sounds","Jimmi Hendrix",
+                12);
+        RetailItem item2 = new DvdMovie("Mad Max", 90, 12);
     }
 }
